10667689c486c89d77b93cf8faecf8f0d76b6487
sabilimaulana <spacysbl.learn@gmail.com>
2022-04-17 20:57:47 +0700

    done(client): Section 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,67 @@ import toast, { Toaster } from "react-hot-toast";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  /*
+   * All state property to store all waves
+   */
+  const [allWaves, setAllWaves] = useState([]);
+
   const [totalWaves, setTotalWaves] = useState(0);
   const [loading, setLoading] = useState(false);
   const [yourWaveTxn, setYourWaveTxn] = useState("");
+
+  const [message, setMessage] = useState("");
 
   const contractAddress = import.meta.env.VITE_WAVEPORTAL_CONTRACT_ADDRESS;
   /**
    * Create a variable here that references the abi content!
    */
   const contractABI = abi.abi;
+
+  /*
+   * Create a method that gets all waves from your contract
+   */
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = waves.map((wave) => ({
+          address: wave.waver,
+          // How to translate timestamp from blockchain to javascript mainstream date
+          timestamp: new Date(wave.timestamp * 1000),
+          message: wave.message,
+        }));
+
+        /*
+         * Store our data in React State
+         */
+
+        console.log(wavesCleaned);
+        setAllWaves(wavesCleaned);
+      } else {
+        toast.error("No ethereum provider found");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const getTotalWaves = () => {
     setLoading(true);
@@ -111,7 +163,9 @@ const App = () => {
         /*
          * Execute the actual wave from your smart contract
          */
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave(message, {
+          gasLimit: 300000,
+        });
         await waveTxn.wait();
         setYourWaveTxn(waveTxn.hash);
 
@@ -127,12 +181,46 @@ const App = () => {
     setLoading(false);
   };
 
-  /*
-   * This runs our function when the page loads.
+  /**
+   * Listen in for emitter events!
+   * Like websockets, we can listen in for events from our contract
    */
   useEffect(() => {
     checkIfWalletIsConnected();
     getTotalWaves();
+    getAllWaves();
+
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
   }, []);
 
   return (
@@ -150,13 +238,22 @@ const App = () => {
               Ethereum wallet and wave at me!
             </div>
 
+            <div className="bio">
+              <small>This program using ethereum Rinkeby Test Network</small>
+            </div>
+            <input
+              type="text"
+              onChange={(e) => setMessage(e.target.value)}
+              className="messageInput"
+            />
             <p className="bio">Total Waves : {totalWaves}</p>
             {yourWaveTxn && (
-              <p className="bio">Your Wave Txn Hash : {yourWaveTxn}</p>
+              <p className="bio">Your last wave txn hash : {yourWaveTxn}</p>
             )}
+
             <button
               className="waveButton"
-              disabled={!currentAccount}
+              disabled={!currentAccount || !message}
               onClick={wave}
             >
               Wave at Me
@@ -170,6 +267,21 @@ const App = () => {
                 Connect Wallet
               </button>
             )}
+
+            {allWaves.map((wave, index) => (
+              <div
+                key={index}
+                style={{
+                  backgroundColor: "OldLace",
+                  marginTop: "16px",
+                  padding: "8px",
+                }}
+              >
+                <div>Address: {wave.address}</div>
+                <div>Time: {wave.timestamp.toString()}</div>
+                <div>Message: {wave.message}</div>
+              </div>
+            ))}
           </div>
         </div>
       )}
